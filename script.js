7c588ecec4db8ba19d29bed850cf905647fb322b
TaylorDarneille <tmdarneille@gmail.com>
2022-09-22 16:45:14 +0300

    updated functionality

--- script.js
+++ script.js
@@ -5,6 +5,19 @@ const generateRandomColor = () => {
     return `rgb(${red}, ${green}, ${blue})`
 }
 
+const addToMyPalette = (event) => {
+    // snatching the color of the clicked div
+    const color = event.target.style.backgroundColor
+    // creating a new div to be added to MYPALETTE
+    const newSquare = document.createElement('div')
+    // add all the good square styles
+    newSquare.classList.add('square')
+    // give the new div the snatched background color
+    newSquare.style.backgroundColor = color
+    // append the new div to my palette
+    document.querySelector('.my-palette').appendChild(newSquare)
+}
+
 const generatePalette = () => {
     console.log("generating palette....")
     // generate 150 divs using a loop
@@ -15,6 +28,7 @@ const generatePalette = () => {
         let color = generateRandomColor()
         square.style.backgroundColor = color
         square.classList.add('square')
+        square.addEventListener('click', addToMyPalette)
         // step 3: append it to an eisting element
         let colorPalette = document.querySelector('.color-palette')
         colorPalette.appendChild(square)
